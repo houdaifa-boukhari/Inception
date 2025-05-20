f15cf859de0ecc14f31c25e0f75afcf7c83ed252
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2025-05-20 16:57:16 -0400

    finished

--- srcs/volumes.sh
+++ srcs/volumes.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 if [ ! -d /home/$USER/data ]; then
-	mkdir -p /home/$USER/MyDatabase/Wordpress
-	mkdir -p /home/$USER/MyDatabase/MariaDB
-	mkdir -p /home/${USER}/logs/fail2ban
-	mkdir -p /home/$USER/logs/vsftpd
-	mkdir -p /home/${USER}/logs/nginx
-	touch /home/$USER/logs/vsftpd/vsftpd.log
-	touch /home/${USER}/logs/nginx/access.log
-	touch /home/${USER}/logs/fail2ban/wordpress.log
-	touch /home/${USER}/logs/fail2ban/vsftpd.log 
+	mkdir -p /home/$USER/data/MyDatabase/Wordpress
+	mkdir -p /home/$USER/data/MyDatabase/MariaDB
+	mkdir -p /home/${USER}/data/logs/fail2ban
+	mkdir -p /home/$USER/data/logs/vsftpd
+	mkdir -p /home/${USER}/data/logs/nginx
+	touch /home/$USER/data/logs/vsftpd/vsftpd.log
+	touch /home/${USER}/data/logs/nginx/access.log
+	touch /home/${USER}/data/logs/fail2ban/wordpress.log
+	touch /home/${USER}/data/logs/fail2ban/vsftpd.log 
 fi
